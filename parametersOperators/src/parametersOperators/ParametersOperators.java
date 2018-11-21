1649d671e693eebfe34f5caf82b22bbb1f05d621
sophrs <45198009+sophrs@users.noreply.github.com>
2018-11-21 13:53:06 +0000

    Add files via upload

---
+++ parametersOperators/src/parametersOperators/ParametersOperators.java
@@ -0,0 +1,13 @@
+package parametersOperators;
+
+public class ParametersOperators {
+
+	public static void main(String[] args) {
+		System.out.println(add(7,6));
+
+	}
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+		
+	}
+}
